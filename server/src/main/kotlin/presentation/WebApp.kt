737980dab8007c7776f430b4d441ece0ecac8684
cola314 <ubj0314@naver.com>
2025-03-02 01:13:18 +0900

    feat: add meta tag in html

--- server/src/main/kotlin/presentation/WebApp.kt
+++ server/src/main/kotlin/presentation/WebApp.kt
@@ -10,6 +10,10 @@ class WebApp(
     private fun HTML.template(block: DIV.() -> Unit) {
         head {
             title("GCV")
+            meta {
+                name = "viewport"
+                content = "width=device-width, initial-scale=1"
+            }
             script { src = "https://unpkg.com/htmx.org@2.0.4" }
             link(rel = "stylesheet", href = "https://cdn.jsdelivr.net/npm/water.css@2/out/water.css")
         }
